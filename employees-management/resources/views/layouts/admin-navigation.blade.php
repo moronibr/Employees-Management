4617a50ed91b764a829f809d649dd0d1355f6d31
moronibr <moroni.ramos@gmail.com>
2023-06-20 05:53:30 -0300

    Uers Index

--- employees-management/resources/views/layouts/admin-navigation.blade.php
+++ employees-management/resources/views/layouts/admin-navigation.blade.php
@@ -1,7 +1,7 @@
 <x-splade-toggle>
     <nav class="bg-white border-b border-gray-100">
         <!-- Primary Navigation Menu -->
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between h-16">
                 <div class="flex">
                     <!-- Logo -->
@@ -9,13 +9,6 @@
                         <Link href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                         </Link>
-                    </div>
-
-                    <!-- Navigation Links -->
-                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                            {{ __('Dashboard') }}
-                        </x-nav-link>
                     </div>
                 </div>
 
